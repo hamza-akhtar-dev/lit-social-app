f8a7fd3befdfba66c001b9f659018ebe6d476580
HamxaCodes <hamzaakhtar8812@gmail.com>
2021-09-02 17:14:26 +0500

    Front End React App - Sidebar Done

--- frontend-react-app/src/pages/home/Home.js
+++ frontend-react-app/src/pages/home/Home.js
@@ -1,8 +1,19 @@
+import "./home.css"
+
+import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
+import Rightbar from "../../components/rightbar/Rightbar";
+import Feed from "../../components/feed/Feed";
+
 export default function Home() {
     return (
-        <div>
+        <>
            <Topbar/>
-        </div>
+           <div className="homeContainer">
+                <Sidebar/>
+                <Feed/>
+                <Rightbar/>
+           </div>
+        </>
     )
 }
