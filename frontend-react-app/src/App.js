572424869efdb5a07317c5ff8c7725df25dc2c7b
HamxaCodes <hamzaakhtar8812@gmail.com>
2021-09-02 21:37:11 +0500

    Front End React App - Completed

--- frontend-react-app/src/App.js
+++ frontend-react-app/src/App.js
@@ -1,11 +1,10 @@
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
-  return (
-    <div>
-      <Home/>
-    </div>
-  );
+  return <Register/>;
 }
 
 export default App;
--- frontend-react-app/src/components/topbar/Topbar.jsx
+++ frontend-react-app/src/components/topbar/Topbar.jsx
@@ -1,39 +1,42 @@
 import "./topbar.css";
-import { Chat, Notifications, Person, Search } from "@material-ui/icons";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 
 export default function Topbar() {
-    return (
-        <div className="topbarContainer">
-            <div className="topbarLeft">
-                <span className="logo">Lit Social</span>
-            </div>
-            <div className="topbarCenter">
-                <div className="searchBar">
-                    <Search className="searchIcon" />
-                    <input type="text" placeholder="Search for friends, posts or videos"  className="searchInput" />
-                </div>
-            </div>
-            <div className="topbarRight">
-                <div className="topbarLinks">
-                    <span className="topbarLink">Homepage</span>
-                    <span className="topbarLink">Timeline</span>
-                </div>
-                <div className="topbarIcons">
-                    <div className="topbarIcon">
-                        <Person className="personIcon"/>
-                        <span className="iconBadge">1</span>
-                    </div>
-                    <div className="topbarIcon">
-                        <Chat className="chatIcon"/>
-                        <span className="iconBadge">2</span>
-                    </div>
-                    <div className="topbarIcon">
-                        <Notifications  className="notificationIcon"/>
-                        <span className="iconBadge">3</span>
-                    </div>
-                </div>
-                <img className="topbarImage" src="./assets/person/1.jpeg" alt="Profile" />
-            </div>
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Lamasocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
         </div>
-    )
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg"/>
+      </div>
+    </div>
+  );
 }
